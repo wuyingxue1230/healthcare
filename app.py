17812fcf74a91ca1f691fee75118a420fb689085
wuyingxue1230 <46773531+wuyingxue1230@users.noreply.github.com>
2024-01-14 18:03:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,64 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+import os
+from LLM import InternLM_LLM
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
 import gradio as gr
+
+def load_chain():
+    # 加载问答链
+    # 定义 Embeddings
+    embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/sentence-transformer")
+
+    # 向量数据库持久化路径
+    persist_directory = 'data_base/vector_db/chroma'
+
+    # 加载数据库
+    vectordb = Chroma(
+        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
+        embedding_function=embeddings
+    )
+
+    # 加载自定义 LLM
+    llm = InternLM_LLM(model_path = "/root/data/model/Shanghai_AI_Laboratory/internlm-chat-7b")
+
+    # 定义一个 Prompt Template
+    template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
+    案。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
+    {context}
+    问题: {question}
+    有用的回答:"""
+
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],template=template)
+
+    # 运行 chain
+    qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectordb.as_retriever(),return_source_documents=True,chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
+    
+    return qa_chain
+    
+
+class Model_center():
+    """
+    存储检索问答链的对象 
+    """
+    def __init__(self):
+        # 构造函数，加载检索问答链
+        self.chain = load_chain()
+
+    def qa_chain_self_answer(self, question: str, chat_history: list = []):
+        """
+        调用问答链进行回答
+        """
+        if question == None or len(question) < 1:
+            return "", chat_history
+        try:
+            chat_history.append(
+                (question, self.chain({"query": question})["result"]))
+            # 将问答结果直接附加到问答历史中，Gradio 会将其展示出来
+            return "", chat_history
+        except Exception as e:
+            return e, chat_history
 
 # 实例化核心功能对象
 model_center = Model_center()
@@ -8,8 +68,8 @@ with block as demo:
     with gr.Row(equal_height=True):   
         with gr.Column(scale=15):
             # 展示的页面标题
-            gr.Markdown("""<h1><center>InternLM</center></h1>
-                <center>书生浦语</center>
+            gr.Markdown("""<h1><center>Renwai</center></h1>
+                <center>健康专家</center>
                 """)
 
     with gr.Row():
