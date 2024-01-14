f1caf6d8c95ab6157109e588306c2922e202f299
wuyingxue1230 <46773531+wuyingxue1230@users.noreply.github.com>
2024-01-14 17:54:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,40 @@
-# 首先导入所需第三方库
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.document_loaders import UnstructuredMarkdownLoader
-from langchain.text_splitter import TokenTextSplitter
-from langchain.vectorstores import Chroma
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from tqdm import tqdm
-import os
+import gradio as gr
 
-# 获取文件路径函数
-def get_files(dir_path):
-    # args：dir_path，目标文件夹路径
-    file_list = []
-    for filepath, dirnames, filenames in os.walk(dir_path):
-        # os.walk 函数将递归遍历指定文件夹
-        for filename in filenames:
-            # 通过后缀名判断文件类型是否满足要求
-            if filename.endswith(".md"):
-                # 如果满足要求，将其绝对路径加入到结果列表
-                file_list.append(os.path.join(filepath, filename))
-            elif filename.endswith(".txt"):
-                file_list.append(os.path.join(filepath, filename))
-    return file_list
+# 实例化核心功能对象
+model_center = Model_center()
+# 创建一个 Web 界面
+block = gr.Blocks()
+with block as demo:
+    with gr.Row(equal_height=True):   
+        with gr.Column(scale=15):
+            # 展示的页面标题
+            gr.Markdown("""<h1><center>InternLM</center></h1>
+                <center>书生浦语</center>
+                """)
 
-# 加载文件函数, tutorial:https://schweitzergao.github.io/2024/01/08/mmtutorial-3-assignment/
-def get_text(dir_path):
-    # args：dir_path，目标文件夹路径
-    # 首先调用上文定义的函数得到目标文件路径列表
-    file_lst = get_files(dir_path)
-    # docs 存放加载之后的纯文本对象
-    docs = []
-    # 遍历所有目标文件
-    for one_file in tqdm(file_lst):
-        file_type = one_file.split('.')[-1]
-        if file_type == 'md':
-            loader = UnstructuredMarkdownLoader(one_file)
-        elif file_type == 'txt':
-            loader = UnstructuredFileLoader(one_file)
-        elif file_type == 'pdf':
-            loader = PyPDFLoader(one_file)
-        else:
-            # 如果是不符合条件的文件，直接跳过
-            continue
-        docs.extend(loader.load())
-    return docs
+    with gr.Row():
+        with gr.Column(scale=4):
+            # 创建一个聊天机器人对象
+            chatbot = gr.Chatbot(height=450, show_copy_button=True)
+            # 创建一个文本框组件，用于输入 prompt。
+            msg = gr.Textbox(label="Prompt/问题")
 
-# 目标文件夹
-tar_dir = [
-    "https://github.com/wuyingxue1230/healthcare/tree/main/files/"
-]
+            with gr.Row():
+                # 创建提交按钮。
+                db_wo_his_btn = gr.Button("Chat")
+            with gr.Row():
+                # 创建一个清除按钮，用于清除聊天机器人组件的内容。
+                clear = gr.ClearButton(
+                    components=[chatbot], value="Clear console")
+                
+        # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
+        db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[
+                            msg, chatbot], outputs=[msg, chatbot])
 
-# 加载目标文件
-docs = []
-for dir_path in tar_dir:
-    docs.extend(get_text(dir_path))
-
-# 对文本进行分块
-text_splitter = TokenTextSplitter(
-    chunk_size=500, chunk_overlap=150)
-split_docs = text_splitter.split_documents(docs)
-
-# 加载开源词向量模型
-embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/sentence-transformer")
-
-# 构建向量数据库
-# 定义持久化路径
-persist_directory = 'data_base/vector_db/chroma'
-# 加载数据库
-vectordb = Chroma.from_documents(
-    documents=split_docs,
-    embedding=embeddings,
-    persist_directory=persist_directory  # 允许我们将persist_directory目录保存到磁盘上
-)
-# 将加载的向量数据库持久化到磁盘上
-vectordb.persist()
+    gr.Markdown("""提醒：<br>
+    1. 初始化数据库时间可能较长，请耐心等待。
+    2. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
+    """)
+gr.close_all()
+# 直接启动
+demo.launch()
